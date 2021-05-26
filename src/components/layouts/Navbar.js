92b8427f953ea0970de8f07f65d496685f933c3c
satyam <satyamonsocial@gmail.com>
2021-05-26 08:18:59 +0530

    preserve users state

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,11 +1,12 @@
+import {Link} from "react-router-dom";
 
 const Navbar = (props) => {
     return(
         <div className="navbar bg-primary" >
             <h2><i className={props.icon}></i> {props.title}</h2>
             <ul>
-                <li><a href="/"> Home</a></li>
-                <li><a href="/about"> About</a></li>
+                <li><Link to="/"> Home</Link></li>
+                <li><Link to="/about"> About</Link></li>
             </ul>
         </div>
     )
